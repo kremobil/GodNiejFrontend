c209e0822c491f92d5fb3fdcd900f8ae23772350
Wiktor Fajkowski <kremobil@wp.pl>
2025-05-30 21:42:32 +0200

    added basic recommended ui

---
+++ src/components/RecomendationCard.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="recomendation-card">
+        <div class="recomendation-card-header">
+            <h3>{{ title }}</h3>
+            <h4>{{ author }}</h4>
+        </div>
+        <div class="recomendation-card-source">
+            <p><b>{{ type === "book" ? "Wydawnictwo:" : "Źródło:" }}</b> {{ source }}</p>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: "RecomendationCard",
+    props: {
+        title: {
+            type: String,
+            default: "Ciąża. Poradnik"
+        },
+        author: {
+            type: String,
+            default: "Karolina Laskowska"
+        },
+        source: {
+            type: String,
+            default: "SBM"
+        },
+        type: {
+            type: String,
+            default: "book"
+        }
+    }
+}
+</script>
+<style scoped>
+.recomendation-card {
+    width: 300px;
+    height: 450px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    gap: 0.5rem;
+    padding: 1rem 2rem;
+    background: linear-gradient(to top, rgba(0, 0, 0, 0.7), transparent 50%),
+        url("@/assets/RecomendationPlaceholder.png") no-repeat center center;
+    border-radius: 1rem;
+}
+
+.recomendation-card-header {
+    display: flex;
+    flex-direction: column;
+}
+
+.recomendation-card-header h3 {
+    font-size: var(--font_m);
+    background: linear-gradient(to right, var(--yellow), var(--magenta));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+}
+
+.recomendation-card-header h4 {
+    font-size: var(--font_s);
+    color: #d4d4d4;
+    font-weight: 400;
+}
+
+.recomendation-card-source p {
+    font-size: var(--font_s);
+    color: #d4d4d4;
+    font-weight: 400;
+}
+</style>
--- src/views/ReadRecommendationsView.vue
+++ src/views/ReadRecommendationsView.vue
@@ -1,20 +1,72 @@
 <script>
 import MainLink from "@/components/MainLink.vue";
+import RecomendationCard from "@/components/RecomendationCard.vue";
 
 export default {
-  name: "ReadRecommendationsView" ,
-  components: {MainLink}
+  name: "ReadRecommendationsView",
+  components: { MainLink, RecomendationCard }
 }
 </script>
 
 <template>
-<div class="read-recommendations-wrapper">
-  <div class="redirect-card">
-    <h1>Zapraszamy niebawem 😊</h1>
-    <h2>Wciąż pracujemy nad tą zakładką by dostarczyć ci jak najlepszej jakości treści.</h2>
-    <MainLink to="/" secondary>Powrót na stronę główną</MainLink>
+  <div class="read-recommendations-wrapper">
+    <h1>Warte Przeczytania</h1>
+    <main>
+      <aside>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla studentów i medyków</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla kobiet w ciąży i połogu</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla mężczyzn</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla mam</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla nastolatek</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla kobiet dojrzałych</button>
+        <button @click="(e) => { e.target.classList.toggle('selected') }">Dla potrzebujących wsparcia</button>
+      </aside>
+      <div class="main_content">
+        <section>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+        </section>
+        <section>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+        </section>
+        <section>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+        </section>
+        <section>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+        </section>
+        <section>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+          <RecomendationCard></RecomendationCard>
+        </section>
+      </div>
+    </main>
   </div>
-</div>
 </template>
 
 <style scoped>
@@ -24,34 +76,59 @@ export default {
   min-height: calc(100vh - 80px - 2rem);
   padding: 4rem;
   flex-direction: column;
-  align-items: center;
-  gap: 6rem;
+  gap: 2rem;
   margin-top: calc(80px + 2rem);
-  justify-content: center;
 }
-.redirect-card {
-  padding: 2rem;
-  border-radius: 1rem;
-  background-color: white;
-  box-shadow: 4px 4px 10px rgba(31, 39, 89, 0.5);
+
+h1 {
+  font-size: var(--font_xl);
+  height: calc((100vh - 80px - 2rem) / 5);
+  background: red;
+  display: flex;
+  align-items: center;
+}
+
+main {
+  display: flex;
+  gap: 2rem;
+  width: 100%;
+}
+
+aside {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  color: var(--blue);
   gap: 2rem;
-  text-align: center;
+  width: 20%;
+  position: sticky;
+  top: calc(80px + 2rem);
 }
 
-.redirect-card h1 {
-  font-size: var(--font_xl);
-}
-.redirect-card h2 {
-  font-size: var(--font_l);
+aside button {
+  background: transparent;
+  color: var(--blue);
+  font-size: var(--font_s);
+  text-align: left;
+  border: none;
+  transition: font-size 0.2s ease-in-out, font-weight 0.2s ease-in-out;
+  cursor: pointer;
+  padding: 0.5rem 0;
 }
 
-@media screen and (max-width: 1024px) {
-  .read-recommendations-wrapper {
-    padding: 2rem;
-  }
+aside button.selected {
+  font-size: var(--font_m);
+  font-weight: 600;
+}
+.main_content {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+}
+.main_content section {
+  display: flex;
+  padding: 2rem;
+  min-height: calc(100vh - 60px - 2rem);
+  flex-wrap: wrap;
+  gap: 2rem;
+  width: 100%;
 }
 </style>
